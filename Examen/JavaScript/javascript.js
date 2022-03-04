85eacb5eda8295df5f22251d095d5d609e2e6826
Rolo3650 <jmoralesc2005@alumno.ipn.mx>
2022-03-04 15:34:22 -0600

    Actualizacion prrona

--- Examen/JavaScript/javascript.js
+++ Examen/JavaScript/javascript.js
@@ -1,3 +1,5 @@
+var texto1 = ""
+
 function calcular(){
     
     var valorAutomovil = formulario.valorAutomovil.value;
@@ -15,10 +17,13 @@ function calcular(){
             if (meses == 3 || meses == 6 || meses == 9 || meses == 12) {
 
                 total1 = (valorAutomovil - cuotaInicial) * ((1 + (interes/100)) ** meses);
-                total2 = Math.round(total1/12);
+                total2 = Math.round((total1+cuotaInicial)/meses);
                 let cuotafinal  = document.getElementById("output");
                 cuotafinal.innerHTML = total2;
-                alert("El precio total del vehículo es de : " + total1 + ", la cuota mensual es de: " + total1/12);
+                alert("El precio total del vehículo es de : " + (total1 + cuotaInicial) + ", la cuota mensual es de: " + (total1 + cuotaInicial)/meses);
+                let tabla = document.getElementById("tablavalores");
+                texto1 += '<tr><td><p class="texto1">'+valorAutomovil+'</p></td><td><p class="texto1">'+porcentaje+' %</p></td><td><p class="texto1">'+meses+'</p></td><td><p class="texto1">3.94%</p></td><td><p class="texto1">'+total2+'</p></td></tr>';
+                tabla.innerHTML = '<tr><td><p class="texto1">Valor de Automovil</p></td><td><p class="texto1">Cuota Inicial</p></td><td><p class="texto1">Periodo</p></td><td><p class="texto1">Interes</p></td><td><p class="texto1">Cuota Mensual</p></td></tr><tr><td><p class="texto2"><input class="input" type="number" name="valorAutomovil" id="valorAutomovil"></p></td><td><p class="texto2"><input class="input" type="number" name="cuotaInicial" id="cuotaIncial"></p></td><td><p class="texto2"><input class="input" type="number" name="meses" id="periodo"></p></td><td><p class="texto2">3,94%</p></td><td><p class="texto2" name="cuotafinal" id="output"></p></td></tr>'+texto1;
 
             } else {
 
